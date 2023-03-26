868ec3b9818bad0e2641656c1ffb03378022b460
GamingReborn <116045383+artemisontop@users.noreply.github.com>
2023-03-25 22:43:35 -0400

    return of a king... April 2023

--- public/js/settings.js
+++ public/js/settings.js
@@ -97,7 +97,7 @@ function checkTheme() {
 function ab() {
 	var myWindow1 = window.open('', 'myWindow1', 'scrollbars=1,height='+screen.availHeight+',width='+screen.availWidth);
 	myWindow1.document.write('<!DOCTYPE html>\n\
-  <title>Google</title>\n\
+  <title>Google Classroom</title>\n\
   <p><iframe src="https://cosmic-production.up.railway.app/" frameborder="0" style="overflow:hidden;overflow-x:hidden;overflow-y:hidden;height:100%;width:100%;position:absolute;top:0px;left:0px;right:0px;bottom:0px" height="100%" width="100%">\n\
   <script>\n\
   alert("");\n\
